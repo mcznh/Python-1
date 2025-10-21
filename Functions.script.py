6ba548bfec0dc2c8ccef86263e671f0724d06325
Maria Carolina Frota <94071764+mcznh@users.noreply.github.com>
2025-10-20 21:06:22 -0300

    Create Functions.script.py

---
+++ Functions.script.py
@@ -0,0 +1,29 @@
+# Find out more about len()
+print(help(len))
+
+# Find out more about int
+print(help(int))
+
+# Find out more about lists
+print(help(list))
+
+course_ratings = {"LLM Concepts": 4.7, 
+                  "Introduction to Data Pipelines": 4.75, 
+                  "AI Ethics": 4.62, 
+                  "Introduction to dbt": 4.81}
+
+# Print the number of key-value pairs
+num_courses = len(course_ratings)
+print(num_courses)
+
+course_completions = [97, 83, 121, 205, 56, 174, 92, 117, 164]
+
+# Find the number of courses
+num_courses = len(course_completions)
+print(num_courses)
+
+most_popular_course = "Introduction to dbt"
+
+# How many characters are in most_popular_course?
+title_length = len(most_popular_course)
+print(title_length)
