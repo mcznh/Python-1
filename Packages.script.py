58db0f623f40e5106be884dc01f5c8f0de70e601
Maria Carolina Frota <94071764+mcznh@users.noreply.github.com>
2025-10-20 23:15:55 -0300

    Update Packages.script.py

--- Packages.script.py
+++ Packages.script.py
@@ -6,3 +6,12 @@ sales_df = pd.DataFrame(sales)
 
 # Preview the first five rows
 print(sales_df.head())
+
+# Read in sales.csv
+sales_df = pd.read_csv("sales.csv")
+
+# Print the mean order_value
+print(sales_df["order_value"].mean())
+
+# Print the total value of sales
+print(sales_df["order_value"].sum())
